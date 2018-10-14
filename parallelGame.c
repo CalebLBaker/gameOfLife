96c1007fad05e0821918302cb3d23b1e8db50b93
CalebLBaker <calebbaker774@gmail.com>
2018-10-14 16:25:54 -0400

    Fix file reading bug

--- parallelGame.c
+++ parallelGame.c
@@ -20,14 +20,18 @@
 
 unsigned id;
 unsigned p;
-char *buffer;
 
 void printMatrix(char *m, size_t height, size_t width) {
 	size_t real_width = width + 2;
-	char *matrix_end = m + (height + 1) * real_width;
+	size_t num_local_rows = BLOCK_SIZE(id,p,height);
+	char *matrix_end = m + (num_local_rows + 1) * real_width;
 	char prompt;
+	static char *buffer = NULL;
 	MPI_Status status;
 	if (id == 0) {
+		if (buffer == NULL) {
+			buffer = malloc(BLOCK_SIZE(p-1,p,height) * width * sizeof(char));
+		}
 		for (char *i = START(m,real_width); i < matrix_end; i += real_width) {
 			char *row_end = i + width;
 			for (char *j = i; j < row_end; j++) {
@@ -36,8 +40,8 @@ void printMatrix(char *m, size_t height, size_t width) {
 			putchar('\n');
 		}
 		for (unsigned i = 1; i < p; i++) {
-			MPI_Send(&prompt, 1, MPI_CHAR, i, PROMPT, MPI_COMM_WORLD);
 			size_t size = BLOCK_SIZE(i,p,height) * width;
+			MPI_Send(&prompt, 1, MPI_CHAR, i, PROMPT, MPI_COMM_WORLD);
 			MPI_Recv(buffer, size, MPI_CHAR, i, PRINT, MPI_COMM_WORLD, &status);
 			char *end = buffer + size;
 			for (char *i = buffer; i < end; i += width) {
@@ -51,13 +55,15 @@ void printMatrix(char *m, size_t height, size_t width) {
 		putchar('\n');
 	}
 	else {
-		size_t size = BLOCK_SIZE(id,p,height) * width;
+		if (buffer == NULL) {
+			buffer = malloc(num_local_rows * width * sizeof(char));
+		}
 		char *dest = buffer;
 		for (char *src = START(m,real_width); src < matrix_end; src += real_width, dest += width) {
 			memcpy(dest, src, width);
 		}
 		MPI_Recv(&prompt, 1, MPI_CHAR, 0, PROMPT, MPI_COMM_WORLD, &status);
-		MPI_Send(buffer, size, MPI_CHAR, 0, PRINT, MPI_COMM_WORLD);
+		MPI_Send(buffer, num_local_rows * width, MPI_CHAR, 0, PRINT, MPI_COMM_WORLD);
 	}
 }
 
@@ -75,9 +81,9 @@ int main(int argc, char **argv) {
 		return 0;
 	}
 
+	// Open input file
 	size_t height, width;
 	FILE *in;
-	// Open input file
 	unsigned reader = p - 1;
 	if (id == reader) {
 		in = fopen(argv[1], "r");
@@ -98,16 +104,20 @@ int main(int argc, char **argv) {
 		MPI_Finalize();
 		return 0;
 	}
-	size_t num_local_rows = BLOCK_SIZE(id,p,height);
 
 	// Allocate memory for matrix. Matrix is represented as a single array, and
 	// we use pointer arithmetic to access individual elements. Note that the
 	// array is larger than the matrix being loaded because we want to leave
 	// space for "sentinal" elements.
+	size_t num_local_rows = BLOCK_SIZE(id,p,height);
 	size_t real_width = width + 2;
 	size_t real_size = (num_local_rows + 2) * real_width;
 	char *next_frame = malloc(real_size * sizeof(char));
 	char *curr_frame = calloc(real_size, sizeof(char));
+	size_t start_index = real_width + 1;
+	unsigned end_index = num_local_rows * real_width + 1;
+	size_t last_row_index = end_index + 1;
+	end_index += real_width;
 
 	if (id == reader) {
 		for (unsigned i = 0; i < reader; i++) {
@@ -119,44 +129,38 @@ int main(int argc, char **argv) {
 			}
 			MPI_Send(next_frame, size, MPI_CHAR, i, INITIAL_READ, MPI_COMM_WORLD);
 		}
-		char *read_end = curr_frame + num_local_rows * real_width;
-		for (char *j = START(curr_frame,real_width); j < read_end; j += real_width) {
+		char *read_end = curr_frame + end_index;
+		for (char *j = curr_frame + start_index; j < read_end; j += real_width) {
 			fread(j, sizeof(char), width, in);
 			getc(in);
 		}
 	}
 	else {
 		MPI_Status status;
-		MPI_Recv(next_frame, num_local_rows * width, MPI_CHAR, reader, INITIAL_READ, MPI_COMM_WORLD, &status);
-		char *read_end = next_frame + num_local_rows * width;
-		char *dest = START(curr_frame,real_width);
+		size_t local_size = num_local_rows * width;
+		MPI_Recv(next_frame, local_size, MPI_CHAR, reader, INITIAL_READ, MPI_COMM_WORLD, &status);
+		char *read_end = next_frame + local_size;
+		char *dest = curr_frame + start_index;
 		for (char *src = next_frame; src < read_end; src += width, dest += real_width) {
 			memcpy(dest, src, width * sizeof(char));
 		}
 	}
 
-	unsigned end_index = (num_local_rows + 1) * real_width;
-	size_t last_row_index = real_width * num_local_rows + 1;
-	unsigned next_guy = id + 1;
-	unsigned prev_guy = id - 1;
-	end_index++;
-
 	char *end = curr_frame + end_index;
-	for (char *i = START(curr_frame,real_width); i < end; i += real_width) {
+	for (char *i = curr_frame + start_index; i < end; i += real_width) {
 		char *row_end = i + width;
 		for (char *j = i; j < row_end; j++) {
 			*j -= '0';
 		}
 	}
 
-	size_t buffer_rows = (id) ? BLOCK_SIZE(id,p,height) : BLOCK_SIZE(reader,p,height);
-	buffer = malloc(buffer_rows * width * sizeof(char));
 	// Clear Sentinals
 	memset(next_frame, 0, real_width);
 	char *end_sentinals = next_frame + end_index;
+	size_t right_sentinal_index = real_width - 1;
 	for (char *i = next_frame + real_width; i < end_sentinals; i += real_width) {
 		*i = 0;
-		i[real_width - 1] = 0;
+		i[right_sentinal_index] = 0;
 	}
 	memset(end_sentinals, 0, real_width);
 
@@ -167,9 +171,10 @@ int main(int argc, char **argv) {
 	// period: the number of cycles between each print
 	unsigned long duration = (size_t) atoi(argv[2]);
 	unsigned long period = (argc == 3) ? -1 : (size_t) atoi(argv[3]);
-
 	bool should_print = argc > 3;
-
+	bool odd = id & 1 == 1;
+	unsigned next_guy = id + 1;
+	unsigned prev_guy = id - 1;
 
 	for (unsigned long turn = 0; turn < duration; turn++) {
 
@@ -179,19 +184,20 @@ int main(int argc, char **argv) {
 		}
 
 		end = curr_frame + end_index;
+		char *curr_frame_start = curr_frame + start_index;
 		MPI_Status status;
-		if (id & 1 == 1) {
+		if (odd) {
 			if (id != reader) {
 				MPI_Send(curr_frame + last_row_index, width, MPI_CHAR, next_guy, SEND_BELOW, MPI_COMM_WORLD);
 				MPI_Recv(end, width, MPI_CHAR, next_guy, SEND_ABOVE, MPI_COMM_WORLD, &status);
 			}
-			MPI_Send(START(curr_frame,real_width), width, MPI_CHAR, prev_guy, SEND_ABOVE, MPI_COMM_WORLD);
+			MPI_Send(curr_frame_start, width, MPI_CHAR, prev_guy, SEND_ABOVE, MPI_COMM_WORLD);
 			MPI_Recv(curr_frame + 1, width, MPI_CHAR, prev_guy, SEND_BELOW, MPI_COMM_WORLD, &status);
 		}
 		else {
 			if (id != 0) {
 				MPI_Recv(curr_frame + 1, width, MPI_CHAR, prev_guy, SEND_BELOW, MPI_COMM_WORLD, &status);
-				MPI_Send(START(curr_frame,real_width), width, MPI_CHAR, prev_guy, SEND_ABOVE, MPI_COMM_WORLD);
+				MPI_Send(curr_frame_start, width, MPI_CHAR, prev_guy, SEND_ABOVE, MPI_COMM_WORLD);
 			}
 			if (id != reader) {
 				MPI_Recv(end, width, MPI_CHAR, next_guy, SEND_ABOVE, MPI_COMM_WORLD, &status);
@@ -200,8 +206,8 @@ int main(int argc, char **argv) {
 		}
 
 		// Fill "next_frame" with values for the next cycle of the game of life
-		char *dest_row = START(next_frame,real_width);
-		for (char *row = START(curr_frame,real_width); row < end; row += real_width) {
+		char *dest_row = next_frame + start_index;
+		for (char *row = curr_frame_start; row < end; row += real_width) {
 			char *row_end = row + width;
 			char *dest = dest_row;
 			for (char *i = row; i < row_end; i++) {
